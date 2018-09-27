764b35cbf59808201e12fc51f0f565a5efa6d33f
SarthakVishnoi01 <sarthakvishnoi.01@gmail.com>
2018-09-27 21:18:45 +0530

    First half Done

--- input.cpp
+++ input.cpp
@@ -17,8 +17,9 @@ int main(){
   vector<pair<int,int> > edgesIn;
   int tempA, tempB;
   /*Reading the input Data*/
-  ifstream read("dataIn.txt");
+  ifstream read("test.graph");
   read >> n >> m >> k;
+
   for(int i=0; i<m; i++){
     read >> tempA >> tempB;
     edgesIn.pb(mp(tempA,tempB));
@@ -46,9 +47,9 @@ int main(){
   ofstream write;
 
   write.open ("inputTemp.cnf");
-  int realVariables = n*k; //
+  int realVariables = n*k + n*n; //
   int variables = realVariables+n*k*(k-1);
-  int clauses = n+k*k*n*(n-1)/2;
+  int clauses = n + (3*n+1)*k*(k-1) + n*n + k*n*(n-1);
   int countZ=1;
   write << "p cnf " << variables << " " << clauses << endl; //Number of variables, Number of clauses
 
@@ -61,16 +62,15 @@ int main(){
   }
 
   /*Subgraph checking*/
-<<<<<<< HEAD
   for(int i=0; i<k; i++){
     for(int j=0; j<k; j++){
       //Checking if i is subgraph of j
       if(i!=j){
         int tempCountZ = countZ;
         for(int x=1; x<=n; x++){
-          write << (realVariables+countZ) << " " << -1*(i*k+x) << " " << -1*(j*k+x) << " 0" << endl; //Z ki definition
-          write << -1*(realVariables+countZ) << " " << (i*k+x) << " 0" << endl;
-          write << -1*(realVariables+countZ) << " " << -1*(j*k+x) << " 0" << endl;
+          write << (realVariables+countZ) << " " << -1*(i*n+x) << " " << -1*(j*n+x) << " 0" << endl; //Z ki definition
+          write << -1*(realVariables+countZ) << " " << (i*n+x) << " 0" << endl;
+          write << -1*(realVariables+countZ) << " " << -1*(j*n+x) << " 0" << endl;
           countZ++;
         }
         for(int x=1; x<=n; x++){
@@ -78,19 +78,8 @@ int main(){
           tempCountZ++;
         }
         write << 0 << endl;
-=======
-
-
-
-  /*Edge connectivity checking*/
-  for(int j=0; j<k; j++){
-    for(int i=1; i<n; i++){
-      for(int l=i+1; l<=n; l++){
-        write << -((n*j)+i) << " " << -((n*j)+l) << " " << (n*k)+((i-1)*n)+l << " ";
->>>>>>> 52fa1169b766241f836ca771d8c73fa65368899f
       }
     }
-    write << 0 << endl;
   }
 
   /*Adding Clauses for edges*/
@@ -98,20 +87,24 @@ int main(){
   for(int i=1; i<=n; i++){
     for(int j=1; j<=n; j++){
       if(edges[i][j]){
-        write << edgesZero+n*i*j << " 0" << endl;
+        write << (edgesZero+n*(i-1)+j) << " 0" << endl;
       }
       else{
-        write << -1*(edgesZero+n*i*j) << " 0" << endl;
+        write << -1*(edgesZero+n*(i-1)+j) << " 0" << endl;
       }
     }
   }
 
-<<<<<<< HEAD
   /*Adding Clauses for full connectivity*/
-  
+  for(int j=0; j<k; j++){
+    for(int i=1; i<=n; i++){
+      for(int l=i+1; l<=n; l++){
+        write << -1*((n*j)+i) << " " << -1*((n*j)+l) << " " << (edgesZero+((i-1)*n)+l) << " 0" << endl;
+      }
+    }
+    // write << 0 << endl;
+  }
 
-=======
->>>>>>> 52fa1169b766241f836ca771d8c73fa65368899f
   write.close();
   cout << "Writing done!" << endl;
 }
